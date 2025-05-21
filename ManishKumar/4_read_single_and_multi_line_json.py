715feee9904c250b02507e1072136a08cb4cd8da
Rishabh Tyagi <30928799+ristyagi@users.noreply.github.com>
2025-05-21 10:18:09 +0530

    Create 4_read_single_and_multi_line_json.py

---
+++ ManishKumar/4_read_single_and_multi_line_json.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 17 23:57:54 2025
+
+@author: Rishabh_Tyagi
+"""
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+        .builder \
+        .appName("read_single_line_json") \
+        .master('local[2]') \
+        .getOrCreate()
+        
+
+# Read single line json        
+df_json = spark.read.format("json") \
+            .option("inferschema", True) \
+            .load("/Users/Rishabh_Tyagi/1_RT_docs/2_My_py_projects/1_Spark_RT/0_datasets/json_datasets/1_single_line.json")
+            
+df_json.show()
+
+
+# Read multi-line json
+df_multi_line_json = spark.read.format("json") \
+    .option("inferschema", True) \
+        .load("/Users/Rishabh_Tyagi/1_RT_docs/2_My_py_projects/1_Spark_RT/0_datasets/json_datasets/2_multi_line.json")
+# Errors out as we have to pass multiline option to read multi-line jsons
+df_multi_line_json.show()
+        
+df_multi_line_json2 = spark.read.format("json") \
+    .option("inferschema", True) \
+        .option("multiline", True) \
+        .load("/Users/Rishabh_Tyagi/1_RT_docs/2_My_py_projects/1_Spark_RT/0_datasets/json_datasets/2_multi_line.json")
+
+df_multi_line_json2.show()
